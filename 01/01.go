b02766024246b6af9bdb6b98ff9a74daada60307
Bharat Agarwal <bharatagarwal@gmail.com>
2022-12-01 22:26:50 +0530

    Adding comments for readability

--- 01/01.go
+++ 01/01.go
@@ -10,17 +10,19 @@ import (
 )
 
 func main() {
+	// read from input file
 	input, err := os.ReadFile("input.txt")
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// split input using double newline as separator
 	var calories []int
-
 	stringInput := string(input[:])
 	caloriesForEachElf := strings.Split(stringInput, "\n\n")
 
+	// parse and add up sets of calories for each elf
 	for _, calorieSets := range caloriesForEachElf {
 		individualCalories := strings.Split(calorieSets, "\n")
 
@@ -36,14 +38,16 @@ func main() {
 		calories = append(calories, total)
 	}
 
+	// sort totals for each elf
 	sortedCalories := make([]int, len(calories))
 	copy(sortedCalories, calories)
 	sort.Ints(sortedCalories)
 
+	// Getting maximum number of calories
 	noOfElves := len(sortedCalories)
-
 	fmt.Printf("Maximum number of calories: %d\n", sortedCalories[noOfElves-1])
 
+	// Getting sum of calories of top three elves
 	sumOfTopThreeElves := sortedCalories[noOfElves-1] +
 		sortedCalories[noOfElves-2] +
 		sortedCalories[noOfElves-3]
